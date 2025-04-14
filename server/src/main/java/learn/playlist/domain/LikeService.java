b304e2a356fd527f987a98e10447c9a431a2a0df
Louis Klein <louisklein71@gmail.com>
2025-04-14 15:14:50 -0700

    like service

---
+++ server/src/main/java/learn/playlist/domain/LikeService.java
@@ -0,0 +1,22 @@
+package learn.playlist.domain;
+
+import learn.playlist.data.LikeRepository;
+import learn.playlist.models.Likes;
+
+import java.util.List;
+
+public class LikeService {
+    private final LikeRepository repository;
+
+    public LikeService(LikeRepository repository) {
+        this.repository = repository;
+    }
+
+    public int countLikesForPlaylist(int playlistId){return repository.countLikesForPlaylist(playlistId);};
+
+    public List<Integer> findLikedPlaylistFromUser(int userId){return repository.findLikedPlaylistIdsByUser(userId);};
+
+    public boolean addLike(Likes likes) {return repository.add(likes);}
+
+    public boolean removeLike(int userId, int playlistId) {return repository.deleteByKey(userId, playlistId);}
+}
