a7ab255df4719fcc065e4d6500e36a3b419923ff
Kevin Brito <06kevin29@gmail.com>
2025-04-14 15:54:06 -0400

    Song repository & tests

---
+++ server/src/main/java/learn/playlist/data/SongJdbcTemplateRepository.java
@@ -0,0 +1,63 @@
+package learn.playlist.data;
+
+import learn.playlist.data.mappers.SongMapper;
+import learn.playlist.models.Song;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.sql.Timestamp;
+
+@Repository
+public class SongJdbcTemplateRepository implements SongRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    public SongJdbcTemplateRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public Song findByUrl(String url) {
+        final String sql = "select song_id, url from song where url = ?;";
+        Song result = jdbcTemplate.query(sql, new SongMapper(), url).stream()
+                .findAny().orElse(null);
+        return result;
+    }
+
+    @Transactional
+    @Override
+    public Song add(Song song) {
+        //If song exists in db already, just return that one
+        Song exists = findByUrl(song.getUrl());
+        if (exists != null) {
+            return exists;
+        }
+
+        final String sql = "insert into song (url) values (?);";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        int rowsAffected = jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, song.getUrl());
+            return ps;
+        }, keyHolder);
+
+        if (rowsAffected <= 0) {
+            return null;
+        }
+
+        song.setSongId(keyHolder.getKey().intValue());
+
+        return song;
+    }
+
+    @Transactional
+    @Override
+    public boolean deleteById(int songId) {
+        jdbcTemplate.update("delete from playlist_song where song_id = ?", songId);
+        return jdbcTemplate.update("delete from song where song_id = ?", songId) > 0;
+    }
+}
---
+++ server/src/test/java/learn/playlist/data/SongJdbcTemplateRepositoryTest.java
@@ -0,0 +1,42 @@
+package learn.playlist.data;
+
+import learn.playlist.models.Song;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+class SongJdbcTemplateRepositoryTest {
+    @Autowired
+    SongJdbcTemplateRepository repository;
+
+    @Autowired
+    KnownGoodState knownGoodState;
+
+    @BeforeEach
+    void setup() {
+        knownGoodState.set();
+    }
+
+    @Test
+    void shouldFindByUrl() {
+        Song song = repository.findByUrl("https://soundcloud.com/example-song1");
+        assertNotNull(song);
+        assertTrue(song.getSongId() > 0);
+    }
+
+    @Test
+    void shouldCreateSong() {
+        Song song = new Song();
+        song.setUrl("https://soundcloud.com/example-song3");
+        assertTrue(repository.add(song).getSongId() > 0);
+    }
+
+    @Test
+    void shouldDeleteSong() {
+        assertTrue(repository.deleteById(2));
+    }
+}
