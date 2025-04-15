d3c778f407ce8ef6fcf814925c973cdc69e8a9b6
rsoto-trinity <78444860+rsoto-trinity@users.noreply.github.com>
2025-04-14 21:28:31 -0500

    playlistservice

---
+++ server/src/main/java/learn/playlist/domain/PlaylistService.java
@@ -0,0 +1,82 @@
+package learn.playlist.domain;
+
+import learn.playlist.data.PlaylistRepository;
+import learn.playlist.models.Playlist;
+import learn.playlist.models.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PlaylistService {
+    private final PlaylistRepository repository;
+
+    public PlaylistService(PlaylistRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Playlist> findAllPublic() {
+        return repository.findAllPublic();
+    }
+
+    public List<Playlist> findByUserId(int userId) {
+        return repository.findByUserId(userId);
+    }
+
+    public List<Playlist> findByUserIdPublic(int userId) {
+        return repository.findByUserIdPublic(userId);
+    }
+
+    public List<Playlist> findByLikes(int userId) {
+        return repository.findByLikes(userId);
+    }
+
+    public Playlist findById(int playlistId) {
+        return repository.findById(playlistId);
+    }
+
+    public Playlist add(Playlist playlist, User user) {
+        if (playlist == null || user == null || user.getRoles() == null) {
+            return null;
+        }
+
+        if (user.getRoles().contains("DISABLED")) {
+            return null;
+        }
+
+        if (!user.getRoles().contains("USER") && !user.getRoles().contains("ADMIN")) {
+            return null;
+        }
+
+        playlist.setUserId(user.getUserId());
+        return repository.add(playlist);
+    }
+
+    public boolean update(Playlist playlist, User user) {
+        if (playlist == null || playlist.getPlaylistId() <= 0 || user == null || user.getRoles() == null) {
+            return false;
+        }
+
+        if (user.getRoles().contains("DISABLED")) {
+            return false;
+        }
+
+        if (!user.getRoles().contains("USER") && !user.getRoles().contains("ADMIN")) {
+            return false;
+        }
+
+        return repository.update(playlist);
+    }
+
+    public boolean deleteById(int playlistId, User user) {
+        if (user == null || user.getRoles() == null) {
+            return false;
+        }
+
+        if (!user.getRoles().contains("ADMIN")) {
+            return false;
+        }
+
+        return repository.deleteById(playlistId);
+    }
+}
